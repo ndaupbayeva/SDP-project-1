ae32cd29ac610680835ffa63c0a7422cbed891ca
Nuriya <230185@astanait.edu.kz>
2024-10-02 21:17:56 +0500

    adding clients code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+                DriverManager driverManager = DriverManager.getInstance();
+                String driver = driverManager.findNearestDriver("Location A");
+                //passenger.assignTaxi(driver);
+            }
     }
-}
